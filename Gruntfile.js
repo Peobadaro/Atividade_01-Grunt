b89ebfa478338afc26eac07f1c5ecd98798df13e
Pedro Badaro <badaro1988@gmail.com>
2025-04-08 20:56:04 +0800

    Corrige estrutura do projeto para usar pasta public

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,119 +3,91 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
+                options: {
+                    paths: ['src/styles']
+                },
                 files: {
-                    'dev/styles/main.css': 'src/styles/main.less'
+                    'public/css/main.css': 'src/styles/main.less'
                 }
             },
             production: {
                 options: {
-                    compress: true,
+                    paths: ['src/styles'],
+                    compress: true
                 },
                 files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less'
+                    'public/css/main.min.css': 'src/styles/main.less'
                 }
             }
         },
         uglify: {
             target: {
                 files: {
-                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                    'public/js/main.min.js': ['src/script/main.js']
                 }
-            }
-        },
-        watch: {
-            less: {
-                files: ['src/styles/**/*.less'],
-                tasks: ['less:development'],
-            },
-            html: {
-
-                files: ['src/index.html'],
-                tasks: ['replace:dev']
-            }
-        },
-        replace: {
-            dev: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
-                        },
-                        {
-                            match: 'ENDERECO_DO_SVG',
-                            replacement: '../src/svg/investment.svg'
-                        },
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/index.html'],
-                        dest: 'dev/'
-                    },
-                ]
-            },
-            dist: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.min.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
-                        },
-                        {
-                            match: 'ENDERECO_DO_SVG',
-                            replacement: './svg/investment.svg'
-                        },
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['prebuild/index.html'],
-                        dest: 'dist/'
-                    },
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/svg/investment.svg'],
-                        dest: 'dist/svg/'
-                    },
-                ]
             }
         },
         htmlmin: {
             dist: {
                 options: {
                     removeComments: true,
-                    collapseWhitespace: true,
+                    collapseWhitespace: true
                 },
                 files: {
-                    'prebuild/index.html': 'src/index.html'
+                    'public/index.html': 'src/index.html'
                 }
             }
         },
-        clean: ['prebuild'],
+        replace: {
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'css/main.css',
+                            replacement: 'css/main.min.css'
+                        },
+                        {
+                            match: 'js/main.js',
+                            replacement: 'js/main.min.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['public/index.html'],
+                        dest: 'public/'
+                    }
+                ]
+            }
+        },
+        clean: {
+            public: ['public/**/*']
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            js: {
+                files: ['src/script/**/*.js'],
+                tasks: ['uglify']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['htmlmin']
+            }
+        }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+    grunt.registerTask('default', ['clean', 'less:development', 'uglify', 'htmlmin', 'replace']);
+    grunt.registerTask('build', ['clean', 'less:production', 'uglify', 'htmlmin', 'replace']);
 }
