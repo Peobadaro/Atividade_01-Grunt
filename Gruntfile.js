adaf0ee0b1afcc666c69bc777827c7904f831637
Pedro Badaro <badaro1988@gmail.com>
2025-04-08 20:53:26 +0800

    Adiciona arquivos compilados e configurações atualizadas

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,164 +1,121 @@
 module.exports = function(grunt) {
-    // Project configuration
     grunt.initConfig({
-        // LESS compilation
+        pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
-                options: {
-                    paths: ['src/less'],
-                    sourceMap: true,
-                    sourceMapFilename: 'dist/css/styles.css.map'
-                },
                 files: {
-                    'dist/css/styles.css': 'src/less/styles.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
             production: {
                 options: {
-                    paths: ['src/less'],
-                    compress: true
+                    compress: true,
                 },
                 files: {
-                    'dist/css/styles.min.css': 'src/less/styles.less'
+                    'dist/styles/main.min.css': 'src/styles/main.less'
                 }
             }
         },
-
-        // CSS minification
-        cssmin: {
-            target: {
-                files: {
-                    'dist/css/styles.min.css': ['dist/css/styles.css']
-                }
-            }
-        },
-
-        // JavaScript minification
         uglify: {
-            options: {
-                mangle: true,
-                compress: true,
-                sourceMap: true
-            },
             target: {
                 files: {
-                    'dist/js/scripts.min.js': ['src/js/**/*.js']
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
                 }
             }
         },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development'],
+            },
+            html: {
 
-        // HTML minification
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true,
-                    minifyJS: true,
-                    minifyCSS: true
-                },
-                files: {
-                    'dist/index.html': 'index.html'
-                }
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
         },
-
-        // HTML replacement
         replace: {
-            dist: {
+            dev: {
                 options: {
                     patterns: [
                         {
-                            match: /styles\.css/g,
-                            replacement: 'styles.min.css'
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
                         },
                         {
-                            match: /scripts\.js/g,
-                            replacement: 'scripts.min.js'
-                        }
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        },
+                        {
+                            match: 'ENDERECO_DO_SVG',
+                            replacement: '../src/svg/investment.svg'
+                        },
                     ]
                 },
                 files: [
                     {
                         expand: true,
                         flatten: true,
-                        src: ['dist/index.html'],
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    },
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        },
+                        {
+                            match: 'ENDERECO_DO_SVG',
+                            replacement: './svg/investment.svg'
+                        },
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
                         dest: 'dist/'
-                    }
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/svg/investment.svg'],
+                        dest: 'dist/svg/'
+                    },
                 ]
             }
         },
-
-        // Connect server configuration
-        connect: {
-            server: {
+        htmlmin: {
+            dist: {
                 options: {
-                    port: 9000,
-                    base: '.',
-                    livereload: true,
-                    open: true
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html'
                 }
             }
         },
+        clean: ['prebuild'],
+    })
 
-        // Watch for changes
-        watch: {
-            options: {
-                // Enable livereload for automatic browser refresh
-                livereload: true,
-                // Don't spawn new processes for better performance
-                spawn: false,
-                // Show notifications for file changes
-                interrupt: true
-            },
-            styles: {
-                files: ['src/less/**/*.less'],
-                tasks: ['less:development'],
-                options: {
-                    // Show which file changed
-                    event: ['changed', 'added', 'deleted']
-                }
-            },
-            // Watch for HTML changes
-            html: {
-                files: ['**/*.html'],
-                tasks: ['htmlmin'],
-                options: {
-                    livereload: true
-                }
-            },
-            // Watch for JavaScript changes
-            scripts: {
-                files: ['src/js/**/*.js'],
-                tasks: ['uglify'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-
-        // Clean task
-        clean: {
-            css: ['dist/css/*'],
-            html: ['dist/*.html'],
-            js: ['dist/js/*']
-        }
-    });
-
-    // Load plugins
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    // Register tasks
-    grunt.registerTask('default', ['clean', 'less:development', 'uglify', 'connect', 'watch']);
-    grunt.registerTask('build', ['clean', 'less:production', 'cssmin', 'uglify', 'htmlmin', 'replace']);
-}; 
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+}
